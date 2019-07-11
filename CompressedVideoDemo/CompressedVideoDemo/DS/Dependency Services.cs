9d09082cdba7ec116363e73bb86b0451d5e070be
Rogério Luís Lobo <matusael5004@hotmail.com>
2019-07-11 12:44:59 -0300

    first commit

---
+++ CompressedVideoDemo/CompressedVideoDemo/DS/Dependency Services.cs
@@ -0,0 +1,19 @@
+﻿using CompressedVideoDemo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CompressedVideoDemo.DS
+{
+    public class Dependency_Services
+    {
+        public static IMobileFeature MobileFeature
+        {
+            get
+            {
+                return DependencyService.Get<IMobileFeature>(DependencyFetchTarget.GlobalInstance);
+            }
+        }
+    }
+}
